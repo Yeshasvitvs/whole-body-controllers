1028e4bc2924864cf8a0ccb905b25255344a9ecd
Yeshasvi Tirupachuri <yeshasvitvs@gmail.com>
2019-02-24 01:29:09 +0100

    Update reference trajectory initial position and add external wrench bash script

---
+++ torque-controllers/end-effector-control/utils/applyEsternalWrench.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+
+################################################################################
+# "MAIN" FUNCTION:                                                             #
+################################################################################
+if [[ $# -eq 0 || $# -lt 1 ]] ; then
+    echo "Error in options passed!"
+    echo ""
+    usage
+    exit 1
+fi
+
+COMMAND=$1
+
+if [[ ${COMMAND} == "x" ]] ; then
+  echo "iCub::r_hand 10 0 0 0 0 0 5" | yarp rpc /iCub/applyExternalWrench/rpc:i
+fi
+
+if [[ ${COMMAND} == "-x" ]] ; then
+  echo "iCub::r_hand -10 0 0 0 0 0 5" | yarp rpc /iCub/applyExternalWrench/rpc:i
+fi
+
+if [[ ${COMMAND} == "y" ]] ; then
+  echo "iCub::r_hand 0 10 0 0 0 0 5" | yarp rpc /iCub/applyExternalWrench/rpc:i
+fi
+
+if [[ ${COMMAND} == "-y" ]] ; then
+  echo "iCub::r_hand 0 -10 0 0 0 0 5" | yarp rpc /iCub/applyExternalWrench/rpc:i
+fi
+
+if [[ ${COMMAND} == "z" ]] ; then
+  echo "iCub::r_hand 0 0 10 0 0 0 5" | yarp rpc /iCub/applyExternalWrench/rpc:i
+fi
+
+if [[ ${COMMAND} == "-z" ]] ; then
+  echo "iCub::r_hand 0 0 -10 0 0 0 5" | yarp rpc /iCub/applyExternalWrench/rpc:i
+fi
+
+exit 0
